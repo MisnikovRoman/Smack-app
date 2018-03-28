d0b579d722841fbcabb8b08c7633a25d6666644d
Roman <misnikovroman@gmail.com>
2018-03-28 14:52:17 +0300

    Add 2 VCs, SWReveal working

---
+++ Smack-app-udemy/Controller/ChatVC.swift
@@ -0,0 +1,22 @@
+//
+//  ChatVC.swift
+//  Smack-app-udemy
+//
+//  Created by Роман Мисников on 28.03.2018.
+//  Copyright © 2018 Роман Мисников. All rights reserved.
+//
+
+import UIKit
+
+class ChatVC: UIViewController {
+
+    @IBOutlet weak var menuButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // manually add action to button
+        menuButton.addTarget(self.revealViewController(), action: #selector(SWRevealViewController.revealToggle(_:)), for: .touchUpInside)
+         
+    }
+}
+
